8129890c3a77dd366a18d065714bfa4000f897ba
David Fuller <david@thetouchpoint.org>
2026-03-18 17:11:42 +0000

    Added first file

--- touchpoint.js
+++ touchpoint.js
@@ -1,6 +1,8 @@
 //Hello
 async function auto_exec(){
   console.log('This is the beginning');
-  await Jade.load_js()
+  await Jade.load_js("https://david-touchpoint.github.io/html_01.js", "html");
+  console.log("HTML loaded");
+  await jade_modules.html.mainHTML();
 
 }
