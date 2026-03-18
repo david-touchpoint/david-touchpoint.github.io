65936718416533ceb03690cefae09b20145665dc
David Fuller <david@thetouchpoint.org>
2026-03-18 16:52:11 +0000

    Lets try this

--- touchpoint.js
+++ touchpoint.js
@@ -1,1 +1,8 @@
 //Hello
+function auto_exec(){
+  // This function is called when the addin opens.
+  // un-comment a line below to take action on open.
+  
+  // Jade.open_automations() // displays a list of functions for a user
+  // Jade.open_editor()      // shows the code editor
+}
