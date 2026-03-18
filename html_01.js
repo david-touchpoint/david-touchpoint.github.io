98d575e8d05e5d7b8c763358db42b1ea4296e527
David Fuller <david@thetouchpoint.org>
2026-03-18 17:23:34 +0000

    Added logo

--- html_01.js
+++ html_01.js
@@ -5,6 +5,7 @@ async function auto_exec(){
 async function mainHTML(params) {
   const html = `
   <h1>Touchpoint Excel</h1>
+  <img src="images/logo-pink.jpeg" alt="pink logo">
   `;
   await Jade.open_canvas("Script-Controller", html, true);
 }
